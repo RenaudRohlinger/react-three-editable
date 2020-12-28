26529afa6258eafe2fa90b02e6a83ba01d4e8dd4
onirenaud <renaud.rohlinger@gmail.com>
2020-12-28 14:31:30 +0900

    added e.primitive

--- src/components/editable.tsx
+++ src/components/editable.tsx
@@ -1,7 +1,7 @@
 import React, {
   ComponentProps,
+  ComponentType,
   forwardRef,
-  JSXElementConstructor,
   RefAttributes,
   useLayoutEffect,
   useRef,
@@ -34,23 +34,24 @@ interface Elements {
 }
 
 const editable = <
-  T extends JSXElementConstructor<any> | EditableType,
-  U extends EditableType
+  T extends ComponentType<any> | EditableType | 'primitive',
+  U extends T extends EditableType ? T : EditableType
 >(
   Component: T,
-  type: U
+  type: T extends 'primitive' ? null : U
 ) =>
   forwardRef(
     (
       {
         uniqueName,
-        position,
-        rotation,
-        scale,
+        visible,
+        editableType,
         ...props
-      }: ComponentProps<T> & {
+      }: Omit<ComponentProps<T>, 'visible'> & {
         uniqueName: string;
-      } & RefAttributes<Elements[U]>,
+        visible?: boolean | 'editor';
+      } & (T extends 'primitive' ? { editableType: U } : {}) &
+        RefAttributes<Elements[U]>,
       ref
     ) => {
       const objectRef = useRef<Elements[U]>();
@@ -64,38 +65,44 @@ const editable = <
 
       ['x', 'y', 'z'].forEach((axis) => {
         transformDeps.push(
-          props[`position-${axis}`],
-          props[`rotation-${axis}`],
-          props[`scale-${axis}`]
+          props[`position-${axis}` as any],
+          props[`rotation-${axis}` as any],
+          props[`scale-${axis}` as any]
         );
       });
 
       useLayoutEffect(() => {
         // calculate initial properties before adding the editable
-        const pos: Vector3 = position
-          ? new Vector3(...position)
+        const pos: Vector3 = props.position
+          ? Array.isArray(props.position)
+            ? new Vector3(...(props.position as any))
+            : props.position
           : new Vector3();
-        const rot: Vector3 = rotation
-          ? new Vector3(...rotation)
+        const rot: Vector3 = props.rotation
+          ? Array.isArray(props.rotation)
+            ? new Vector3(...(props.rotation as any))
+            : props.rotation
           : new Vector3();
-        const scal: Vector3 = scale
-          ? new Vector3(...scale)
+        const scal: Vector3 = props.scale
+          ? Array.isArray(props.scale)
+            ? new Vector3(...(props.scale as any))
+            : props.scale
           : new Vector3(1, 1, 1);
 
         ['x', 'y', 'z'].forEach((axis, index) => {
-          if (props[`position-${axis}`])
-            pos.setComponent(index, props[`position-${axis}`]);
-          if (props[`rotation-${axis}`])
-            rot.setComponent(index, props[`rotation-${axis}`]);
-          if (props[`scale-${axis}`])
-            scal.setComponent(index, props[`scale-${axis}`]);
+          if (props[`position-${axis}` as any])
+            pos.setComponent(index, props[`position-${axis}` as any]);
+          if (props[`rotation-${axis}` as any])
+            rot.setComponent(index, props[`rotation-${axis}` as any]);
+          if (props[`scale-${axis}` as any])
+            scal.setComponent(index, props[`scale-${axis}` as any]);
         });
 
         const quaternion = new Quaternion().setFromEuler(
           new Euler().setFromVector3(rot)
         );
 
-        addEditable(type, uniqueName, {
+        addEditable(type ?? editableType, uniqueName, {
           transform: new Matrix4().compose(pos, quaternion, scal),
         });
 
@@ -105,11 +112,11 @@ const editable = <
         // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [
         addEditable,
-        position,
         removeEditable,
-        rotation,
-        scale,
         uniqueName,
+        props.position,
+        props.rotation,
+        props.scale,
 
         // nasty
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -152,10 +159,12 @@ const editable = <
         <Component
           ref={mergeRefs([objectRef, ref])}
           {...props}
+          visible={visible !== 'editor' && visible}
           userData={{
             __editable: true,
             __editableName: uniqueName,
-            __editableType: type,
+            __editableType: type ?? editableType,
+            __visibleOnlyInEditor: visible === 'editor',
           }}
         />
       );
@@ -163,8 +172,10 @@ const editable = <
   );
 
 const createEditable = <T extends EditableType>(type: T) =>
+  // @ts-ignore
   editable(type, type);
 
+editable.primitive = editable('primitive', null);
 editable.group = createEditable('group');
 editable.mesh = createEditable('mesh');
 editable.spotLight = createEditable('spotLight');
